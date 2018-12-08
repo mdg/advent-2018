bc31f348ed7036bc6e8064c373190f00b2fc362b
Matthew Graham <mdg149@gmail.com>
2018-12-07 23:18:48 -0500

    add update the guards script
    parsing and finding guards sleeping and learning lots of new stuff
    about rust libraries

--- 04/guards.rs
+++ 04/guards.rs
@@ -3,9 +3,11 @@
 extern crate lazy_static;
 extern crate regex;
 
-use regex::RegexSet;
+use regex::Regex;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, Read};
+use std::ops::Range;
 use std::str::FromStr;
 
 
@@ -23,8 +25,8 @@ fn read_input(input_filename: &str) -> io::Result<String>
 enum Event
 {
     BeginsShift(i32),
-    Sleeps(i32),
-    Wakes(i32),
+    Sleeps(i16),
+    Wakes(i16),
 }
 
 impl FromStr for Event
@@ -34,35 +36,116 @@ impl FromStr for Event
     fn from_str(input: &str) -> io::Result<Event>
     {
         lazy_static! {
-            static ref RE: RegexSet = RegexSet::new(&[
-                // r#"^.+:([0-9]{2}) (Guard #([0-9]+) begins|falls a(sleep)|(wake)s up).*$"#
-                r#"^.+:([0-9]{2}\]) "#,
-                r#"^.+ Guard.*$"#,
-                r#"^.+ falls asleep$"#,
-                r#"^.+ wakes up$"#,
-            ]).unwrap();
+            static ref RE: Regex = Regex::new(
+                r#"^.+:([0-9]{2})\] (Guard #([0-9]+) begins|falls a(sleep)|(wake)s up).*$"#
+            ).unwrap();
         }
-        let matches = RE.captures(input);
-        if !matches.matched_any() {
+        let opt_matches = RE.captures(input);
+        if opt_matches.is_none() {
             return Err(io::Error::new(io::ErrorKind::InvalidData, input));
         }
-println!("matches: {:?}", matches);
-        Ok(Event::BeginsShift(0))
+        let matches = opt_matches.as_ref().unwrap();
+        let minute_match = matches.get(1).unwrap();
+        let guard_match = matches.get(3);
+        let sleep_match = matches.get(4);
+
+        let minute_str = minute_match.as_str();
+        let minute = i16::from_str_radix(minute_str, 10).unwrap();
+
+        let ev = if guard_match.is_some() {
+            let guard_str = guard_match.unwrap().as_str();
+            let id = i32::from_str_radix(guard_str, 10).unwrap();
+            Event::BeginsShift(id)
+        } else if sleep_match.is_some() {
+            Event::Sleeps(minute)
+        } else {
+            Event::Wakes(minute)
+        };
+        Ok(ev)
     }
 }
 
+#[derive(Clone)]
+#[derive(Copy)]
+#[derive(Debug)]
+struct Sleep
+{
+    id: i32,
+    sleep: i16,
+    wake: i16,
+}
+
+impl Sleep
+{
+    pub fn range(&self) -> Range<usize>
+    {
+        let first = self.sleep as usize;
+        let second = self.wake as usize;
+        first..second
+    }
+}
+
+#[derive(Debug)]
 struct Guard
 {
     id: i32,
-    sleeps: [i16; 60],
+    sleeps: Vec<i16>,
+}
+
+impl Guard
+{
+    pub fn new(s: Sleep) -> Guard
+    {
+        Guard {
+            id: s.id,
+            sleeps: vec![0; 60],
+        }
+    }
+
+    pub fn add_sleep(&mut self, s: Sleep)
+    {
+        for i in s.range() {
+            self.sleeps[i] += 1;
+        }
+    }
 }
 
 fn main() -> io::Result<()>
 {
     let input = read_input("sorted_input")?;
-    for line in input.lines() {
-        let ev: Event = line.parse()?;
-        println!("{:?}", ev);
+    let events_result: io::Result<Vec<Event>> = input
+        .lines()
+        .map(|line| {
+            line.parse()
+        })
+        .collect();
+    let events = events_result?;
+    let mut sleeps = Vec::with_capacity(events.len());
+    let mut guard_id = 0;
+    let mut sleep_time = 0;
+    for ev in events {
+        match ev {
+            Event::BeginsShift(id) => {
+                guard_id = id;
+            }
+            Event::Sleeps(minute) => {
+                sleep_time = minute;
+            }
+            Event::Wakes(minute) => {
+                sleeps.push(Sleep {
+                    id: guard_id,
+                    sleep: sleep_time,
+                    wake: minute,
+                });
+            }
+        }
     }
+
+    let mut guards = HashMap::new();
+    for s in sleeps {
+        let mut g = guards.entry(s.id).or_insert(Guard::new(s));
+        g.add_sleep(s);
+    }
+
     Ok(())
 }
